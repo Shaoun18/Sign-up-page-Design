98219ca2cc817a95ee5de71c32fe654fcc5e9635
Shaoun18 <shaounchandrashill75@gmail.com>
2021-12-29 21:13:00 +0600

    Signup page Design

---
+++ app/src/main/java/com/seip/lt2b1design/MainActivity.java
@@ -0,0 +1,14 @@
+package com.seip.lt2b1design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
